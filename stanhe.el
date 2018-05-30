b2e2c36734554147d2992adcc3978bb73520a772
卜萝胡白 <hshl4314@gmail.com>
2018-05-30 19:21:04 +0800

    remove custom

--- stanhe.el
+++ stanhe.el
@@ -20,6 +20,9 @@
        (interactive)
        (switch-to-buffer nil))
 
+(setq org-log-done 'note)
+(setq org-agenda-files '("~/org/"))
+
 ;init packages
 (when (>= emacs-major-version 24)
   (setq package-archives '(("gnu" . "http://elpa.emacs-china.org/gnu/")
@@ -202,18 +205,3 @@
         "gs" 'magit-status
 
     ))
-
-;;custom settings
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.1)
- '(company-minimum-prefix-length 1))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
