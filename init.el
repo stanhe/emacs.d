761aed6ca812a32989b8f953a56a468abfd73fdd
yellow dog <hshl4314@126.com>
2018-08-10 09:03:51 +0800

    refactor init file

--- init.el
+++ init.el
@@ -7,20 +7,12 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
+;; custom file
+(setq custom-file "~/.emacs.d/lisp/custom.el")
+(load custom-file)
+
 ;;init with org-file
 (org-babel-load-file (expand-file-name "stanhe.org" user-emacs-directory))
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(eww-header-line-format "%u"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- lisp/custom.el
+++ lisp/custom.el
@@ -4,8 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.3)
- '(company-minimum-prefix-length 2))
+ '(eww-header-line-format "%u"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
