f549a4691d9b8b0422de34642aa64845d20dc107
stanhe <hshl4314@gmail.com>
2018-09-17 16:49:34 +0800

    add jedi

--- lisp/custom.el
+++ lisp/custom.el
@@ -36,7 +36,7 @@
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (color-theme-sanityinc-tomorrow sublime-themes organic-green-theme calmer-forest-theme green-screen-theme use-package neotree ace-window company hungry-delete counsel evil hydra general smartparens which-key window-numbering popwin monokai-theme counsel-projectile magit keychain-environment multi-term clojure-mode clj-refactor cider paredit js2-mode gh-md markdown-mode)))
+    (jedi color-theme-sanityinc-tomorrow sublime-themes organic-green-theme calmer-forest-theme green-screen-theme use-package neotree ace-window company hungry-delete counsel evil hydra general smartparens which-key window-numbering popwin monokai-theme counsel-projectile magit keychain-environment multi-term clojure-mode clj-refactor cider paredit js2-mode gh-md markdown-mode)))
  '(pos-tip-background-color "#FFFACE")
  '(pos-tip-foreground-color "#272822")
  '(vc-annotate-background nil)
