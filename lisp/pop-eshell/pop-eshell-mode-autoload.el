b76d70fd7d85120d1420d61a07c6301a18b4b6d8
stanhe <hshl4314@126.com>
2018-10-30 20:53:30 +0800

    fix .gitignore

---
+++ lisp/pop-eshell/pop-eshell-mode-autoload.el
@@ -0,0 +1,36 @@
+;;; pop-eshell-mode-autoload.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "pop-eshell-mode" "pop-eshell-mode.el" (23512
+;;;;;;  20601 327031 897000))
+;;; Generated autoloads from pop-eshell-mode.el
+
+(autoload 'fast-eshell-pop "pop-eshell-mode" "\
+fast jump to eshll,it's the same as M-x :eshell 
+
+\(fn)" t nil)
+
+(autoload 'eshell-pop-toggle "pop-eshell-mode" "\
+pop eshell or hide.
+
+\(fn)" t nil)
+
+(autoload 'pop-eshell-mode "pop-eshell-mode" "\
+my pop eshell mode
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pop-eshell-mode" '("get-" "pre-pa" "my-")))
+
+;;;***
+
+(provide 'pop-eshell-mode-autoload)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pop-eshell-mode-autoload.el ends here
