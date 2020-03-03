a24817ece588ba3cb93d55ac04bcab33449a9105
stan <hshl4314@gmail.com>
2020-03-03 11:47:15 +0800

    adjust timer toast

--- lisp/my-test.el
+++ lisp/my-test.el
@@ -3,14 +3,14 @@
 (defun notify (time msg)
   "notify at time .see `run-at-time' and `message-box'"
   (interactive)
-  (run-at-time time nil #'notify_py msg)
-  (concat "Toast at:" time))
+  (concat "Toast at:" time)
+  (run-at-time time nil #'notify_py msg))
 
 (defun notify_hour (msg)
-  "notify every hour"
+  "notify every 1 hour"
   (interactive)
-  (let ((interval (* 3 3600)))
-    (run-at-time interval interval #'notify_py msg)) ;;notify every 3 hour
+  (let ((interval (* 1 3600)))
+    (run-at-time interval interval #'notify_py msg)) ;;notify every 1 hour
   ;;(run-at-time 5 10 #'notify_py msg)
   )
 
@@ -19,13 +19,18 @@
 
 ;;for rest my eyes
 (defun start-rest-timer()
+  "for rest my eyes!"
   (interactive)
   (cancel-rest-timer)
   (setq mRestTimer (notify_hour "have a rest!")))
 
 (defun cancel-rest-timer()
+  "stop reset timer!"
   (interactive)
   (ignore-errors
     (cancel-timer mRestTimer)))
 
 (start-rest-timer)
+(notify "11:00" "请开始点餐！！！")
+(notify "17:30" "请注意下班打卡！！！")
+(run-at-time "17:30" nil #'cancel-rest-timer)
