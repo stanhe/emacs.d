756543bac3dea011ba62c00555aae94bae075749
stan <hshl4314@gmail.com>
2020-03-13 12:20:07 +0800

    add sth-defun

--- lisp/my-abbrev.el
+++ lisp/my-abbrev.el
@@ -15,19 +15,60 @@
 					try-complete-lisp-symbol-partially
 					try-complete-lisp-symbol))
 ;; skeleton	    
+(defvar *skeleton-markers* nil
+  "Markers for locations saved in skeleton-positions")
+
+(add-hook 'skeleton-end-hook 'skeleton-make-markers)
+
+(defun skeleton-make-markers ()
+  (while *skeleton-markers*
+    (set-marker (pop *skeleton-markers*) nil))
+  (setq *skeleton-markers*
+	(mapcar 'copy-marker (reverse skeleton-positions))))
+
+(defun skeleton-next-position (&optional reverse)
+  "Jump to next position in skeleton.
+         REVERSE - Jump to previous position in skeleton"
+  (interactive "P")
+  (let* ((positions (mapcar 'marker-position *skeleton-markers*))
+	 (positions (if reverse (reverse positions) positions))
+	 (comp (if reverse '> '<))
+	 pos)
+    (when positions
+      (if (catch 'break
+	    (while (setq pos (pop positions))
+	      (when (funcall comp (point) pos)
+		(throw 'break t))))
+	  (goto-char pos)
+	(goto-char (marker-position
+		    (car *skeleton-markers*)))))))
+
 (define-skeleton 1src
     "Input src"
     ""
     "#+BEGIN_SRC emacs-lisp \n"
-    _ "\n"
-    "#+END_SRC")
+    >"    "_ ?\n
+    >"#+END_SRC")
+
 (define-skeleton 1java
     "Input src"
     ""
-    "#+HEADER: :classname\n"
-    "#+BEGIN_SRC java \n"
-    _ "\n"
-    "#+END_SRC")
+    "#+HEADER: :classname T1"?\n
+    >"#+BEGIN_SRC java"?\n
+    >"  class T1{" ?\n
+    > -2"    public static void main(String[] args){"?\n
+    > -4"      System.out.println(\"test1\");"_ ?\n
+    > -6"  }}"?\n
+    > -2"#+END_SRC"?\n)
+
+(define-skeleton sth-defun
+  "define my function by skeleton"
+  >"(defun " (skeleton-read "Function name:")"("("Param: " str " ") ")" \n
+  >"\"" (skeleton-read "Docstring: ") "\"" \n
+  >(if (y-or-n-p "interactive?:") "\(interactive\)\n" "")
+  >"(" _ "))" \n
+)
+
 
 (clear-abbrev-table global-abbrev-table)
 ;; (define-abbrev org-mode-abbrev-table "isrc" "" '1src)
