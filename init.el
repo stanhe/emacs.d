fc87d89d05f6a4a1c9dab78af607e01943056e12
stanhe <hshl4314@gmail.com>
2020-03-19 11:51:10 +0800

    indent code

--- init.el
+++ init.el
@@ -21,7 +21,8 @@
   :init
   (setq pop-find-parent-directory '(".git" "gradlew" ".projectile"))
   :commands (fast-eshell-pop eshell-pop-togger)
-  :bind ("C-l" . eshell/mclear))
+  :config
+  (add-hook 'eshell-mode-hook (lambda () (local-set-key (kbd "C-l") #'sth-eshell-clear))))
 
 ;;(run-with-idle-timer 1 nil (lambda () (sth-theme-by-time)))
 (run-with-idle-timer 1 nil (lambda () (sth-loading 5 "Loading Emacs plugins...")))
