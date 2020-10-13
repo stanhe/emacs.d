2e279cd294105fcc27d331366a3f74a28293b2aa
stanhe <hshl4314@gmail.com>
2020-10-13 11:38:58 +0800

    spead up

--- init.el
+++ init.el
@@ -7,20 +7,33 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 (setq byte-compile-warnings '(cl-functions))
+(let (
+      ;; 加载的时候临时增大`gc-cons-threshold'以加速启动速度。
+      (gc-cons-threshold most-positive-fixnum)
+      ;; 清空避免加载远程文件的时候分析文件。
+      (file-name-handler-alist nil))
+  (require 'benchmark-init-modes)
+  (require 'benchmark-init)
+  (benchmark-init/activate)
 
-;;init with org-file.
-(org-babel-load-file (expand-file-name "stanhe.org" user-emacs-directory))
+    ;; ===> 配置 开始
 
-;;add all my custom modes.
-(init-my-load-path)
-(choose-theme-by-time)
-(use-package gradle-mode
-  :defer 3)
-(use-package pop-eshell-mode
-  :defer 3
-  :init
-  (setq pop-find-parent-directory '(".git" "gradlew"))
-  :config
-  (pop-eshell-mode))
+  ;;init with org-file.
+  (org-babel-load-file (expand-file-name "stanhe.org" user-emacs-directory))
 
-;;(run-with-idle-timer 1 nil (lambda () (_loading 1 "loading...." (load-theme 'monokai))))
+  ;;add all my custom modes.
+  (init-my-load-path)
+  (choose-theme-by-time)
+  (use-package gradle-mode
+    :defer 3)
+  (use-package pop-eshell-mode
+    :defer 3
+    :init
+    (setq pop-find-parent-directory '(".git" "gradlew"))
+    :config
+    (pop-eshell-mode))
+
+  ;;(run-with-idle-timer 1 nil (lambda () (_loading 1 "loading...." (load-theme 'monokai))))
+
+    ;; ===> 配置 结束 
+)
